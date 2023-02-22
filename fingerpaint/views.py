d5c90e147b71c2a85b0352b03f60924a9055dba8
wekwas <90058284+wekwas@users.noreply.github.com>
2023-02-21 18:27:34 -0600

    Implemented basic login view

--- fingerpaint/views.py
+++ fingerpaint/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import *
+from classes import myUser
 
-# Create your views here.
+
+class Login(View):
+    def get(self, request):
+        return render(request, "?????.html", {})
+
+    def post(self, request):
+        attempted_username = request.POST['username']
+        attempted_password = request.POST['password']
+        if not myUser.exists(attempted_username):
+            return render(request, "?????.html", {"message": "Incorrect username"})
+        else:
+            my_user = myUser.get_user(attempted_username)
+            my_password = myUser.get_password(my_user)
+        if not myUser.password_check(attempted_password, my_password):
+            return render(request, "?????.html", {"message": "Incorrect password"})
+        else:
+            request.session["session_username"] = attempted_username
+            request.session.set_expiry(0)
+            return redirect("/?????/")
+
+
+class Homepage(View):
+    def get(self, request):
+        my_user = myUser.get_user(request.session["session_username"])
+        return render(request, "?????.html", {})
+
+    def post(self, request):
+        my_user = myUser.get_user(request.session["session_username"])
+        return render(request, "?????.html", {})
