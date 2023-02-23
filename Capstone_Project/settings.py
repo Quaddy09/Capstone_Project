7cc10590a55fba12767014b94e368bbf915878ef
jrya99 <jakeryan@uwm.edu>
2023-02-23 12:30:09 -0600

    linked static css

--- Capstone_Project/settings.py
+++ Capstone_Project/settings.py
@@ -117,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_DIRS = [BASE_DIR,  "Capstone_Project/static"]
 STATIC_URL = 'static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
